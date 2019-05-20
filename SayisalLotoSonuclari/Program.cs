2fd2ca8295d7f984c61d907864b9e87a44e5a122
Armand Cloud <lazcop61@hotmail.com>
2019-05-20 13:10:13 +0300

    Update

--- SayisalLotoSonuclari/Program.cs
+++ SayisalLotoSonuclari/Program.cs
@@ -6,14 +6,7 @@ namespace SayisalLotoSonuclari
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
             SeleniumManager seleniumManager = new SeleniumManager();
-            //FileOperations fileOperations = new FileOperations();
-           // fileOperations.ConvertCsvToJason();
-           // Console.ReadLine();
         }
     }
 }
